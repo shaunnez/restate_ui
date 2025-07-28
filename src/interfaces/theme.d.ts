b7ce16d00eb9135813e461db26520d56cbaaf1df
shaunnesbittiag <shaun.nesbitt@iag.co.nz>
2025-07-28 21:04:28 +1200

    inintg issues

--- src/interfaces/theme.d.ts
+++ src/interfaces/theme.d.ts
@@ -1,15 +1,17 @@
 import "@refinedev/mui";
+import type {
+  Theme as RefineTheme,
+  ThemeOptions as RefineThemeOptions,
+} from "@refinedev/mui";
 
 export interface CustomTheme {
-  // Add custom variables here like below:
+  // Add custom variables here
   // status: {
   //   danger: string;
   // };
 }
 
 declare module "@refinedev/mui" {
-  interface Theme extends import("@refinedev/mui").Theme, CustomTheme {}
-  interface ThemeOptions
-    extends import("@refinedev/mui").ThemeOptions,
-      CustomTheme {}
+  interface Theme extends RefineTheme, CustomTheme {}
+  interface ThemeOptions extends RefineThemeOptions, CustomTheme {}
 }
